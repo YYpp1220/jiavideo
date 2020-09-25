29a9821a1fa61d8d7106e88dcdbe5e43e66d7b30
mymrhui <mymrhui@diao.com>
2020-09-26 02:10:49 +0800

    1.jiavideo注册中心创建 2.完善注册中心，加上注解，以及添加logback

--- jv-register/src/main/java/com/jiavideo/JvRegisterApplication.java
+++ jv-register/src/main/java/com/jiavideo/JvRegisterApplication.java
@@ -1,17 +1,24 @@
 package com.jiavideo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * 启动类
  * @author Lenovo
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class JvRegisterApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JvRegisterApplication.class);
 
     public static void main(String[] args){
         // 运行spring应用
         SpringApplication.run(JvRegisterApplication.class, args);
+        LOG.info("启动成功");
     }
 }
