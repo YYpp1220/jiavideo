d0a4cd9c303701fd80758a3cff8fd1e93f0e9517
mymrhui <mymrhui@diao.com>
2020-09-26 01:19:30 +0800

    jiavideo注册中心创建

---
+++ jv-register/src/main/java/com/jiavideo/JvRegisterApplication.java
@@ -0,0 +1,17 @@
+package com.jiavideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author Lenovo
+ */
+@SpringBootApplication
+public class JvRegisterApplication {
+
+    public static void main(String[] args){
+        // 运行spring应用
+        SpringApplication.run(JvRegisterApplication.class, args);
+    }
+}
