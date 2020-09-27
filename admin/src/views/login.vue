89a6c730149aee0c464645def90e18746c22bfa7
mymrhui <mymrhui@diao.com>
2020-09-27 23:27:52 +0800

    1.增加login登录跳转admin页面 2.使用mounted组件解决样式问题

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -52,7 +52,8 @@
                                                     </label>
 
                                                     <button type="button"
-                                                            class="width-35 pull-right btn btn-sm btn-primary">
+                                                            class="width-35 pull-right btn btn-sm btn-primary"
+                                                            @click="login()">
                                                         <i class="ace-icon fa fa-key"></i>
                                                         <span class="bigger-110">登录</span>
                                                     </button>
@@ -73,8 +74,17 @@
 </template>
 
 <script>
-    $('body').attr('class', 'login-layout light-login');
     export default {
-        name: "login"
+        name: "login",
+        mounted: function() {
+            $('body').removeClass('no-skin');
+            $('body').attr('class', 'login-layout light-login');
+            //console.log("login");
+        },
+        methods: {
+            login() {
+                this.$router.push("/admin")
+            }
+        },
     }
 </script>
