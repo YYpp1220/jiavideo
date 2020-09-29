13f2c528bac113cd3e9d109e513b521890919eed
mymrhui <mymrhui@diao.com>
2020-09-29 19:57:20 +0800

    1.新增chapter页面以及子路由，并用假数据制作表格

--- jv-business/src/main/java/com/jiavideo/business/controller/admin/ChapterController.java
+++ jv-business/src/main/java/com/jiavideo/business/controller/admin/ChapterController.java
@@ -1,11 +1,15 @@
-package com.jiavideo.business.controller;
+package com.jiavideo.business.controller.admin;
 
+import com.jiavideo.business.dto.ChapterDTO;
 import com.jiavideo.business.entity.Chapter;
 import com.jiavideo.business.server.ChapterServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 章控制器
@@ -14,13 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/09/28
  */
 @RestController
+@RequestMapping("/admin")
 public class ChapterController {
     @Autowired
     private ChapterServer chapterServer;
 
-    @GetMapping("/queryById")
-    public ResponseEntity<Chapter> queryById(){
-        Chapter chapter = chapterServer.queryById();
-        return ResponseEntity.ok(chapter);
+    /**
+     * 查询所有
+     *
+     * @return {@link ResponseEntity<List<ChapterDTO>>}
+     */
+    @GetMapping("/queryAll")
+    public ResponseEntity<List<ChapterDTO>> queryAll(){
+        List<ChapterDTO> chapterDTOList = chapterServer.queryAll();
+        return ResponseEntity.ok(chapterDTOList);
     }
 }
--- jv-business/src/main/java/com/jiavideo/business/server/ChapterServer.java
+++ jv-business/src/main/java/com/jiavideo/business/server/ChapterServer.java
@@ -1,10 +1,17 @@
 package com.jiavideo.business.server;
 
+import cn.hutool.json.JSONUtil;
+import com.jiavideo.business.dto.ChapterDTO;
 import com.jiavideo.business.entity.Chapter;
+import com.jiavideo.business.entity.ChapterExample;
 import com.jiavideo.business.mapper.ChapterMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 章服务器
@@ -18,7 +25,14 @@ public class ChapterServer {
     @Autowired(required = false)
     private ChapterMapper chapterMapper;
 
-    public Chapter queryById() {
-        return chapterMapper.selectByPrimaryKey("1");
+    /**
+     * 查询所有
+     * @return {@link List<ChapterDTO>}
+     */
+    public List<ChapterDTO> queryAll() {
+        ChapterExample example = new ChapterExample();
+        List<Chapter> chapterList = chapterMapper.selectByExample(example);
+        List<ChapterDTO> chapterDTOList = chapterList.stream().map(chapter -> JSONUtil.toBean(JSONUtil.toJsonStr(chapter), ChapterDTO.class)).collect(Collectors.toList());
+        return chapterDTOList;
     }
 }
